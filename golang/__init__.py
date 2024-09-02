529733326ddf2b932e5402d847983522ac54bbb6
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-09-02 22:28:38 +0530

    Update __init__.py

--- golang/__init__.py
+++ golang/__init__.py
@@ -1,1 +1,8 @@
+go mod tidy   
 
+$env:CC="C:\android-ndk-r27\toolchains\llvm\prebuilt\windows-x86_64\bin\armv7a-linux-androideabi21-clang"
+$env:CGO_ENABLED="1"  
+$env:GOARCH="arm"
+$env:GOOS="android"  
+
+go build -o majorbin_arm .  
