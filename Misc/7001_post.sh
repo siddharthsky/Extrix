29d03921acb7f63e6655b95dec4c73b15e340b35
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2026-05-09 18:46:35 +0530

    Create 7001_post.sh

---
+++ Misc/7001_post.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export SHELL=/bin/bash
+
+curl -O https://raw.githubusercontent.com/chrisward/scripts/main/setup_termux.sh && bash setup_termux.sh
+
+
+echo "Installation Completed"
+sleep 5
